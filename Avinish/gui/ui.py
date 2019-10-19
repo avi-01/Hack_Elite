3bc4b5e6e52be5830eb4cc06c8d4106dee903f25
Shubham Kumar <scheleon08@gmail.com>
2019-10-19 12:34:05 -0700

    App upgraded

--- Avinish/gui/ui.py
+++ Avinish/gui/ui.py
@@ -6,6 +6,27 @@ back = '#454647'
 titleBack = '#343b80'
 startClick = 0
 
+r =tk.Tk()
+contentFrame = Frame(r, bg=back)
+class eyeDetails :
+	def __init__(self) :
+		global contentFrame
+		contentFrame.destroy()
+		contentFrame = Frame(r, bg=back)
+		title = Label(contentFrame, text = "Eye Details")
+		title.pack(side = TOP)
+		contentFrame.pack(side = TOP, fill=BOTH, expand=True)
+		contentFrame.tkraise()
+
+
+class history :
+	def __init__(self) :
+		global contentFrame
+		contentFrame.destroy()
+		contentFrame = Frame(r, bg=back)
+		title = Label(contentFrame, text = "History")
+		title.pack(side = TOP)
+		contentFrame.pack(side = TOP,fill=BOTH,expand=True)
 
 def onOFF():
     global startClick 
@@ -22,9 +43,6 @@ def start():
 def stop():
     onOFFButton.configure(text='Start')
 
-
-
-r = tk.Tk() 
 r.title('Destello')
 r.configure(background=back)
 r.geometry("800x600")
@@ -32,8 +50,35 @@ r.geometry("800x600")
 titleFrame = Frame(r,bg=titleBack)
 titleFrame.pack(side=TOP, fill=X,ipady=2)
 
-title = Label(titleFrame,text="Destello",bg=titleBack,fg="white",font=('Woodcut', 18,'italic'),height=2)
+title = Label(titleFrame,text="Destello",bg=titleBack,fg="white",font=('Woodcut', 18,'italic'))
 title.pack(fill=X)
+
+topdown = Frame(r,height=10)
+eyeHealthButton = Button(topdown, text = 'Eye Health',width=20,height=2,bg="green",relief=SUNKEN,fg='white',font=(None,11,'bold'),border=0, command=eyeDetails) # add command to execute
+eyeHealthButton.pack(side=LEFT, padx = 8, pady = 10, fill=X)
+
+seperator = Label(topdown, text = ' History ',width=20,height=2,bg="green",relief=SUNKEN,fg='white',font=(None,11,'bold'),border=0)
+seperator.pack(side = LEFT, padx = 8, pady = 10)
+
+historyOptions = StringVar(r)
+historyOptions.set("Select") # default value
+
+def on_field_change(*args):
+	print("value changed to " + historyOptions.get())
+	history()
+historyOptions.trace('w', on_field_change)
+
+historyDropdown = OptionMenu(topdown, historyOptions, "Past 24 hrs", "Past 30 days ") # add command to execute
+historyDropdown.pack(side = LEFT, padx = 8, pady = 10)
+addExceptionAppButton = Button(topdown, text = "Add exception app") # add command to execute
+addExceptionAppButton.pack(side=LEFT, padx = 8, pady = 3)
+
+logoutButton = Button(topdown, text = "Logout") # add command to execute
+logoutButton.pack(side=LEFT, padx = 8, pady = 3, fill=X)
+
+topdown.pack(side=TOP, fill=X)
+
+
 
 
 bottomFrame = Frame(r,bg=back,padx=20,pady=30)
@@ -41,4 +86,4 @@ onOFFButton = Button(bottomFrame,text='Start',width=20,height=2,bg=titleBack,fg=
 onOFFButton.pack(side=RIGHT)
 bottomFrame.pack(side=BOTTOM,fill=X)
 
-r.mainloop() 
+r.mainloop()
--- scheleon/getProcess.py
+++ scheleon/getProcess.py
@@ -1,3 +1,4 @@
+
 import subprocess
 import re
 import requests
@@ -9,26 +10,27 @@ for name in file:
 	name = re.sub('\n', '', name)
 	allowedProcesses.append(name)
 print(allowedProcesses)
+while(1):
+	processNameSource = subprocess.Popen(["powershell","gps | ? {$_.mainwindowtitle} | select name | ft -AutoSize"],stdout=subprocess.PIPE)
 
-processNameSource = subprocess.Popen(["powershell","gps | ? {$_.mainwindowtitle} | select name | ft -AutoSize"],stdout=subprocess.PIPE)
+	processNames = processNameSource.communicate()[0].decode('ascii').strip().split('\r\n')
 
-processNames = processNameSource.communicate()[0].decode('ascii').strip().split('\r\n')
-
-for ii in range(0, len(processNames) - 1) :
-    processNames[ii] = re.sub(' +', '', processNames[ii])
+	for ii in range(0, len(processNames) - 1) :
+		processNames[ii] = re.sub(' +', '', processNames[ii])
     
 # removed Column titles : 'Names' from the list
-del processNames[:2]
+	del processNames[:2]
 
-processesToCLose = []
-for ii in range(0, len(processNames)-1) :
-	runningProcess = processNames[ii]
-	result = [allowedProcessName for allowedProcessName in allowedProcesses if(allowedProcessName.lower() in runningProcess.lower() or runningProcess.lower() in allowedProcessName.lower())]
-	if not bool(result) :
-	    processesToCLose.append(runningProcess)
+	processesToCLose = []
+	for ii in range(0, len(processNames)-1) :
+		runningProcess = processNames[ii]
+		result = [allowedProcessName for allowedProcessName in allowedProcesses if(allowedProcessName.lower() in runningProcess.lower() or runningProcess.lower() in allowedProcessName.lower())]
+		if not bool(result) :
+			processesToCLose.append(runningProcess)
 
-jsonStringProcessesToClose = json.dumps(processesToCLose, separators=(',', ':'))
-print(jsonStringProcessesToClose)
+	jsonStringProcessesToClose = json.dumps(processesToCLose, separators=(',', ':'))
+	print(jsonStringProcessesToClose)
 
-if bool(processesToCLose) :
-    requests.post(url = "http://localhost:3001", jsonStringProcessesToClose)
+	print(not bool(processesToCLose))
+# if bool(processesToCLose) :
+#     requests.post(url = "http://localhost:3001", jsonStringProcessesToClose)
