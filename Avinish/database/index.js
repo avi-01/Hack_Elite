3adb62ce51fbdc1276a2912966445170f645010c
avi-01 <avnishmay@gmail.com>
2019-10-19 13:45:26 +0530

    Added Api

---
+++ Avinish/database/index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+require('./mongoose')
+const blinkRouter = require('./routers/blink')
+const timeRouter = require('./routers/time')
+const userRouter = require('./routers/user')
+const historyRouter = require('./routers/history')
+
+const port = 3001
+const app = express()
+
+
+app.use(express.json())
+app.use(blinkRouter)
+app.use(timeRouter)
+app.use(userRouter)
+app.use(historyRouter)
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello")
+})
+app.listen(port, ()=>{
+    console.log("Server running on "+port)
+})
