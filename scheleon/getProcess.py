83700378caf7cdf808f471cba5ec342e70bfaed4
Shubham Kumar <41225915+scheleon@users.noreply.github.com>
2019-10-18 20:16:44 -0700

    Update getProcess.py

--- scheleon/getProcess.py
+++ scheleon/getProcess.py
@@ -11,32 +11,24 @@ for name in file:
 print(allowedProcesses)
 
 processNameSource = subprocess.Popen(["powershell","gps | ? {$_.mainwindowtitle} | select name | ft -AutoSize"],stdout=subprocess.PIPE)
-processMainWindowTitleSource = subprocess.Popen(["powershell","gps | ? {$_.mainwindowtitle} | mainwindowtitle | ft -AutoSize"],stdout=subprocess.PIPE)
 
 processNames = processNameSource.communicate()[0].decode('ascii').strip().split('\r\n')
-processMainWindowTitles = processMainWindowTitleSource.communicate()[0].decode('ascii').strip().split('\r\n')
-
-# removed Column titles : 'Names' from the list
-del processNames[0-2]
-del processMainWindowTitles[0-2]
 
 for ii in range(0, len(processNames) - 1) :
     processNames[ii] = re.sub(' +', '', processNames[ii])
     
-for ii in range(0, len(processMainWindowTitles) - 1) :
-    processMainWindowTitles[ii] = re.sub(' +', '', processMainWindowTitles[ii])
+# removed Column titles : 'Names' from the list
+del processNames[:2]
 
 processesToCLose = []
 for ii in range(0, len(processNames)-1) :
-	runningProcess = []
-	runningProcess.append(processNames[ii])
-	runningProcess.append(processMainWindowTitles[ii])
-	result = [allowedProcessName for allowedProcessName in allowedProcesses if(allowedProcessName.lower() in runningProcess[0].lower() or runningProcess[0].lower() in allowedProcessName.lower())]
+	runningProcess = processNames[ii]
+	result = [allowedProcessName for allowedProcessName in allowedProcesses if(allowedProcessName.lower() in runningProcess.lower() or runningProcess.lower() in allowedProcessName.lower())]
 	if not bool(result) :
-		processesToCLose.append(runningProcess)
+	    processesToCLose.append(runningProcess)
 
 jsonStringProcessesToClose = json.dumps(processesToCLose, separators=(',', ':'))
 print(jsonStringProcessesToClose)
 
-# if bool(processesToCLose)
-# 	requests.post(url = "http://localhost:3001", jsonStringProcessesToClose)
+if bool(processesToCLose) :
+    requests.post(url = "http://localhost:3001", jsonStringProcessesToClose)
